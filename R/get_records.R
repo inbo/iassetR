a01593536d4501725d524cf87e5ad2567b4d0eb0
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-11-10 13:17:33 +0100

    create get_records()

---
+++ R/get_records.R
@@ -0,0 +1,29 @@
+#' Title
+#'
+#' @param inspection_name name of the custom inspection to return records for
+#' @param access_token access token from `get_access_token()`
+#'
+#' @return
+#' @export
+#'
+#' @examples
+get_records <- function(inspection_name = "Vespa-Watch",
+                        access_token = get_access_token()) {
+  # check input params
+  assertthat::assert_that(assertthat::is.string(access_token))
+  assertthat::assert_that(assertthat::is.string(inspection_name))
+  # get the inspection_id for the custom inspection
+  inspection_fields <- get_fields(access_token = access_token,
+                                  name = inspection_name)
+  # build a request and perform it
+  get_records_request <-
+    httr2::request("https://api.iasset.nl/getCustomInspections") %>%
+    httr2::req_body_form(
+      access_token = access_token,
+      inspection_ids[0] = inspection_fields$id
+    )
+  records <- httr2::req_perform(get_records_request) %>%
+    httr2::resp_body_json(check_type = FALSE)
+
+  return(records)
+}

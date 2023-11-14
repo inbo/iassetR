35246714e66fd12ce2fd19778b54598ca01938ed
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-11-14 14:06:26 +0100

    Create `check_request_success()`: forward api errors

---
+++ R/utils.R
@@ -0,0 +1,19 @@
+#' Helper to check if a Request was successful
+#'
+#' @param parsed_response
+#'
+#' @return `TRUE` for a successful request, otherwise, an error.
+#'
+#' @examples
+check_request_success <- function(parsed_response) {
+  # If the request is successful, the response will have `error$FALSE` otherwise,
+  # it'll contain a message
+  assertthat::assert_that(isFALSE(purrr::pluck(parsed_response, "error")),
+    msg = paste0(
+      "Something went wrong while accessing the API:\n",
+      purrr::pluck(parsed_response, "error", "error_msg")
+    )
+  )
+  # This will only get evaluated if the above assertion passes.
+  return(TRUE)
+}

84f39c0b64dbb5c6ce65902302a63d7509cf89c8
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-11-14 16:16:56 +0100

    set family, don't render rd

--- R/utils.R
+++ R/utils.R
@@ -3,10 +3,10 @@
 #' @param parsed_response
 #'
 #' @return `TRUE` for a successful request, otherwise, an error.
-#'
-#' @examples
+#' @family helper functions
+#' @noRd
 check_request_success <- function(parsed_response) {
-  # If the request is successful, the response will have `error$FALSE` otherwise,
+  # If the request is successful, the response will have `error$FALSE`otherwise,
   # it'll contain a message
   assertthat::assert_that(isFALSE(purrr::pluck(parsed_response, "error")),
     msg = paste0(

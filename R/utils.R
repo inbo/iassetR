6c43a7665c65d028fa8356e0c6d20e3a3bd47473
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-11-14 16:17:10 +0100

    init helper to check for valid access token shape

--- R/utils.R
+++ R/utils.R
@@ -17,3 +17,17 @@ check_request_success <- function(parsed_response) {
   # This will only get evaluated if the above assertion passes.
   return(TRUE)
 }
+
+#' Helper to assert that the access_token has the right shape
+#'
+#' @param access_token
+#'
+#' @return `TRUE` for a string in the shape of an access token,
+#' otherwise an error
+#' @family helper functions
+#' @noRd
+is_access_token <- function(access_token) {
+  assertthat::assert_that(assertthat::is.string(access_token))
+  assertthat::assert_that(nchar(access_token) == 120)
+  assertthat::assert_that(grepl("^[-A-Za-z0-9+\\/]*={0,3}$", access_token))
+}
